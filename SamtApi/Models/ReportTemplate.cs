bd3427f5e03c03210dffcb99cde3048510a821b2
navidras <navidras@gmail.com>
2022-06-16 19:04:06 +0430

    Excel Report Class

--- SamtApi/Models/ReportTemplate.cs
+++ SamtApi/Models/ReportTemplate.cs
@@ -4,8 +4,19 @@ namespace SamtApi.Models {
 		public string PersianDate { get; set; }
 		public string DayName { get; set; }
 
-		public List<PersonTemplate> _personTemplates { get; set; }=new List<PersonTemplate>();
+		//public List<PersonTemplate> _personTemplates { get; set; }=new List<PersonTemplate>();
+		public  List<TheShift> Shifts { get; set; }
 		
+	}
+
+	public class TheShift {
+		public string ShiftName { get; set; }	
+		public List<ThePerson> ThePersonList { get; set; }=new List<ThePerson>();
+	}
+
+	public class ThePerson {
+		public string Name { get; set; }
+		public string ResourceName { get; set; }
 	}
 
 	public class PersonTemplate {
