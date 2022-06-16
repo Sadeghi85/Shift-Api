0c2a5518dcf6276f97fedc0ca1b51d23416320ec
navidras <navidras@gmail.com>
2022-06-16 10:02:44 +0430

    غلط دیکته

--- src/Leopard.Bussiness/Model/ReturnModel/ShiftResultModel.cs
+++ src/Leopard.Bussiness/Model/ReturnModel/ShiftResultModel.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Leopard.Bussiness.Model.ReturnModel {
-	public class ShiftResultModel {
+	public class ShiftResultModel  {
 
 		public int Id { get; set; } // ID (Primary key)
 		public string Title { get; set; } // Title (length: 250)
 		public int? PortalId { get; set; } // PortalId
 		public TimeSpan? StartTime { get; set; } // StartTime
 		public TimeSpan? EndTime { get; set; } // EndTime
-		public string PotalTitle { get; set; }	
+		public string PortalTitle { get; set; }	
+		public int? ShiftTypeId { get; set; }	
+		public string ShiftTypeTitle { get; set; }
 	}
 }
