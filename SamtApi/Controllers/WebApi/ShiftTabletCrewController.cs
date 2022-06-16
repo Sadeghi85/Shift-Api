80ec0e9e87f44b7c71f78682ca71f016cf3058ba
navidras <navidras@gmail.com>
2022-06-16 09:58:34 +0430

    Result for Main Report

--- SamtApi/Controllers/WebApi/ShiftTabletCrewController.cs
+++ SamtApi/Controllers/WebApi/ShiftTabletCrewController.cs
@@ -3,6 +3,8 @@ using Leopard.Bussiness.Model.ReturnModel;
 using Leopard.Bussiness.Services.Interface;
 using Leopard.Repository;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using SamtApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -35,11 +37,80 @@ namespace SamtApi.Controllers.WebApi {
 		// GET: api/<ShiftTabletCrewController>
 		[HttpPost("GetAll")]
 		public async Task<IActionResult> GetAll(ShiftTabletCrewSearchModel model) {
-			List<ShiftTabletCrewSearchResult>? res = await _shiftTabletCrewService.GetAll(model);
+			List<ShfitTabletReportResult>? res = await _shiftTabletCrewService.GetAll(model);
 			if (res.Count() > 0) {
-				return Ok(OperationResult<List<ShiftTabletCrewSearchResult>?>.SuccessResult(res, _shiftTabletCrewService.GetAllCount()));
+				return Ok(OperationResult<List<ShfitTabletReportResult>?>.SuccessResult(res, _shiftTabletCrewService.GetAllCount()));
 			}
 			return Ok(OperationResult<string>.FailureResult(""));
+		}
+
+		[HttpPost("GetGeExcel")]
+		public async Task<IActionResult> GetGeExcel(ShiftTabletCrewSearchModel model) {
+
+			List<ShfitTabletReportResult>? res = await _shiftTabletCrewService.GetAll(model);
+
+			var dates = res.Select(pp => new { pp.PersianDate, pp.PersianWeekDay }).Distinct().ToList();
+
+			List<ReportTemplate> reportTemplates = new List<ReportTemplate>();
+			
+			foreach (var i in dates) {
+
+				var tmp = res.Where(pp => pp.PersianDate == i.PersianDate).Select(pp=> new PersonTemplate { Name=pp.firstName+" "+pp.lastName, ResourceName= pp.jobName, Shift= pp.shiftTitle  } ).ToList();
+				var tt = new ReportTemplate { PersianDate = i.PersianDate, DayName = i.PersianWeekDay, _personTemplates=tmp };
+
+				reportTemplates.Add(tt);
+
+
+
+			}
+
+
+
+			var stream = new MemoryStream();
+
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+			using (var package = new ExcelPackage(stream)) {
+
+				var ws = package.Workbook.Worksheets.Add("Contracts Report");
+
+
+				var row = 1;
+				//foreach (var i in res) {
+				//	ws.Cells["A" + row].Value = i.shiftTitle;
+				//	ws.Cells["B" + row].Value = i.firstName;
+				//	ws.Cells["C" + row].Value = i.lastName;
+				//	ws.Cells["D" + row].Value = i.PersianDate;
+				//	ws.Cells["E" + row].Value = i.jobName;
+				//	ws.Cells["F" + row].Value = i.PersianWeekDay;
+
+				//	row++;
+				//}
+
+				foreach(var i in reportTemplates) {
+
+					ws.Cells["A" + row].Value = i.PersianDate;
+					ws.Cells["B" + row].Value = i.DayName;
+
+					row++;
+				}
+
+
+
+
+
+
+
+				ws.View.RightToLeft = true;
+				package.SaveAs(stream);
+				stream.Position = 0;
+
+
+			}
+			return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"Report.xlsx");
+
+
+			//return Ok();
 		}
 
 		// GET api/<ShiftTabletCrewController>/5
@@ -54,7 +125,7 @@ namespace SamtApi.Controllers.WebApi {
 
 		// POST api/<ShiftTabletCrewController>
 		[HttpPost("Register")]
-		public async Task<IActionResult> Register( ShiftTabletCrewModel model) {
+		public async Task<IActionResult> Register(ShiftTabletCrewModel model) {
 			var res = await _shiftTabletCrewService.Register(model);
 
 			if (res > 0) {
@@ -75,7 +146,7 @@ namespace SamtApi.Controllers.WebApi {
 		}
 
 		[HttpPost("Replace/{replaced}/{replacedBy}")]
-		public async Task<IActionResult> Replace(int replaced , int replacedBy) {
+		public async Task<IActionResult> Replace(int replaced, int replacedBy) {
 			var res = await _shiftTabletCrewService.Replace(replaced, replacedBy);
 			if (res > 0) {
 				return Ok(OperationResult<int>.SuccessResult(res));
