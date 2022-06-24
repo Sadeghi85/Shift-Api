2b54c044fe6024898e5641385d4a78c28b4ff705
ahmadmolaie <molaie@gmail.com>
2022-06-24 17:00:32 +0430

    we talked earlier, enter & exit should be nullable

--- src/Leopard.Bussiness/Model/ShiftTabletCrewModel.cs
+++ src/Leopard.Bussiness/Model/ShiftTabletCrewModel.cs
@@ -7,18 +7,21 @@ using System.Threading.Tasks;
 
 namespace Leopard.Bussiness.Model {
 	public class ShiftTabletCrewModel {
+
 		public int Id { get; set; } // ID (Primary key)
+
 		[Required(ErrorMessage ="شناسه کارمند اجباری است")]
 		public int AgentId { get; set; } // AgentId
 
-		[Required(ErrorMessage = "شناسه عنوان اجباری است")]
+		[Required(ErrorMessage = "سمت اجباری است")]
 		public int ResourceTypeId { get; set; } // ResourceId
+
 		[Required(ErrorMessage = "شناسه لوح اجباری است")]
 		public int ShiftTabletId { get; set; } // ShifTabletId
-		[Required(ErrorMessage = "تاریخ ورود اجباری است")]
-		public DateTime EntranceTime { get; set; } // EntranceTime
-		[Required(ErrorMessage = "تاریخ خروج اجباری است")]
-		public DateTime ExitTime { get; set; } // ExitTime
+		
+		public DateTime? EntranceTime { get; set; } // EntranceTime
+	
+		public DateTime? ExitTime { get; set; } // ExitTime
 		
 		public bool IsReplaced { get; set; } // IsReplaced
 	}
