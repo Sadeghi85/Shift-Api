9f8fe8412b609e9ea8db1dc55ec71cd3ed13440d
navidras <navidras@gmail.com>
2022-06-25 17:07:04 +0430

    task 6567

---
+++ src/Leopard.Bussiness/Model/ShiftDateStartEnd.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leopard.Bussiness.Model {
+	internal class ShiftDateStartEnd {
+		public DateTime StartDateTime { get; set; } = new DateTime();
+		public DateTime EndDateTime { get; set; } = new DateTime();
+	}
+}
